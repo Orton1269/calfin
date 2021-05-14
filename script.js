e7871db8522fa3d458df824955a65fed055d6ab3
Orton1269 <ikundu66@gmail.com>
2021-05-15 00:45:53 +0530

    done

---
+++ script.js
@@ -0,0 +1,33 @@
+function dis(val){
+    document.getElementById("result").value+=val;
+    console.log(val);
+    }
+    function solve(){
+        var x=document.getElementById("result").value;
+        var y=eval(x);
+        document.getElementById("result").value=y;
+    }
+    function clr()
+    {
+        document.getElementById("result").value="";
+    }
+    function back()
+    {
+        value = document.getElementById("result").value;
+        document.getElementById("result").value = value.substr(0, value.length - 1);
+    }
+
+    
+const toggleSwitch = document.querySelector(
+    '.theme-switch input[type="checkbox"]'
+  );
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }
+  
+  toggleSwitch.addEventListener("change", switchTheme, false);
